755804bbc7d0dc34abff9248f1616ab9ca9a9aaa
hoorayitt <hoorayitt@gmail.com>
2019-02-21 21:24:42 +0800

    iff @  tiantianjiangdeMacBook-Air.local in 19-02-21=21:24:42

--- bin/setting.sh
+++ bin/setting.sh
@@ -4,8 +4,6 @@ echo '以下配置内容可多次执行'
 echo '请按任意键继续......'
 read -n1
 
-TODO_bin=$(cd "$(dirname "$0")";pwd)
-cd $TODO_bin
 read -p  '是否需要多版本存储功能 [y/N]' version
 if [ 'y' = $version ]; then
 	read -p  '是否需要云端同步功能 [y/N]' remote
@@ -19,4 +17,6 @@ if [ 'y' = $version ]; then
 else
 	echo '你配置了Todo-in-Vim极简版'
 	cp ./.vimrc_tiny ../.vimrc_for_todo
+	echo "cp ./.vimrc_tiny ../.vimrc_for_todo"
 fi
+
